9139700a49d0c656fdee1e29d8317b822ef1ca74
Tianyi Wang <wang103uiuc@gmail.com>
2013-04-24 12:08:20 -0500

    Aligned dynamic allocation done

--- kernel/kheap.c
+++ kernel/kheap.c
@@ -25,6 +25,10 @@ static void insert_node_into_rbtree(struct rb_node *new_node) {
     rb_insert_fixup(new_node, &mem_root);
 }
 
+/**
+ * Split the node according to the needed size. If the splitting happens, a new
+ * node will be inserted after the provided node.
+ */
 static void split_node(mem_node *node, uint32_t needed_size) {
     if (node->size > needed_size + sizeof(mem_node)) {
         mem_node *new_node = (mem_node *)(((char *)node) + sizeof(mem_node)
@@ -51,15 +55,47 @@ static mem_node *_find_available_node(struct rb_node *node, uint32_t size,
     if (cur_mem_node->in_use == 0 && align) {
         uint32_t location = (uint32_t)cur_mem_node;
 
-        uint32_t offset = 0;
         if ((location + sizeof(mem_node)) & MM_ALIGN_4K) {
-            offset = PAGE_SIZE - (location + sizeof(mem_node)) % PAGE_SIZE;
+            uint32_t offset = PAGE_SIZE -
+                (location + sizeof(mem_node)) % PAGE_SIZE;
             
-            uint32_t real_size = cur_mem_node->size - offset;
+            int real_size = cur_mem_node->size - offset;
 
-            if (real_size >= size) {
-                return cur_mem_node;
+            if (real_size >= 0 &&
+                (uint32_t)real_size >= size &&
+                offset >= sizeof(mem_node)) {
+
+                mem_node *new_mem_node = (mem_node *)
+                    ((char *)cur_mem_node + offset);
+
+                // Update the sizes.
+                new_mem_node->size = real_size;
+                cur_mem_node->size -= (real_size + sizeof(mem_node));
+
+                // Add the new node to the rb tree.
+                insert_node_into_rbtree(&new_mem_node->node);
+
+                return new_mem_node;
             }
+
+            real_size -= PAGE_SIZE;
+            if (real_size >= 0 && (uint32_t)real_size >= size) {
+               
+                mem_node *new_mem_node = (mem_node *)
+                    ((char *)cur_mem_node + offset+ PAGE_SIZE);
+
+                // Update the sizes.
+                new_mem_node->size = real_size;
+                cur_mem_node->size -= (real_size + sizeof(mem_node));
+
+                // Add the new node to the rb tree.
+                insert_node_into_rbtree(&new_mem_node->node);
+
+                return new_mem_node;
+            }
+        }
+        else if (cur_mem_node->size >= size) {
+            return cur_mem_node;
         }
     }
     else if (cur_mem_node->in_use == 0 && cur_mem_node->size >= size) {
@@ -81,8 +117,30 @@ static mem_node *find_available_node(uint32_t size, int align) {
 
     if (node == NULL) {
         // Need to expand the boundary of heap for more memory.
+        
         uint32_t i = kheap_cur_end;
-        uint32_t new_bound = kheap_cur_end + size + sizeof(mem_node);
+        uint32_t new_node_end = i + sizeof(mem_node);
+
+        if (align && (new_node_end & MM_ALIGN_4K)) {
+            uint32_t offset = PAGE_SIZE - new_node_end % PAGE_SIZE;
+            
+            struct rb_node *last_rb_node = maximum(mem_root.rb_node);
+            mem_node *last_mem_node = rb_entry(last_rb_node, mem_node, node);
+            
+            last_mem_node->size += offset;
+            kheap_cur_end += offset;
+
+            while (i < kheap_cur_end) {
+                page *pg = get_page(i, 1, kernel_directory);
+                alloc_frame(pg, 1, 1);
+
+                i += PAGE_SIZE;
+            }
+
+            i = kheap_cur_end;
+        }
+
+        uint32_t new_bound = i + sizeof(mem_node) + size;
 
         while (i < new_bound) {
             page *pg = get_page(i, 1, kernel_directory);
@@ -90,6 +148,7 @@ static mem_node *find_available_node(uint32_t size, int align) {
 
             i += PAGE_SIZE;
         }
+
         node = (mem_node *)kheap_cur_end;
         kheap_cur_end = new_bound;
 
