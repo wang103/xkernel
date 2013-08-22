acad862286b48f5e56130fd67daf81a8eefed22f
Tianyi Wang <wang103uiuc@gmail.com>
2013-08-21 17:38:23 -0700

    Added scheduler

--- kernel/scheduler.c
+++ kernel/scheduler.c
@@ -3,3 +3,15 @@
 void init_scheduler() {
     init_threading();
 }
+
+void thread_is_ready(thread_t *thread) {
+
+}
+
+void thread_is_not_ready(thread_t *thread) {
+
+}
+
+void schedule() {
+
+}
--- kernel/scheduler.h
+++ kernel/scheduler.h
@@ -4,5 +4,8 @@
 #include "thread.h"
 
 void init_scheduler();
+void thread_is_ready(thread_t *thread);
+void thread_is_not_ready(thread_t *thread);
+void schedule();
 
 #endif
